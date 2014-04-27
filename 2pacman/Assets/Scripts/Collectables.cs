da8de1cd01ad4cf9d28e8e3980f3704904d1efe9
miemiebumlekat <mieskouboe@hotmail.com>
2014-04-28 00:36:38 +0200

    Collectables
    Coins to collect... I dunno

---
+++ 2pacman/Assets/Scripts/Collectables.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectables : MonoBehaviour {
+
+
+	int score = 0;
+	// Function: 
+	public void OnCollisionEnter( Collision other ) {
+		if (other.gameObject.name == "coin") {
+						score += 5;
+						Destroy (other.gameObject);
+						Debug.Log ("Mie");
+						Debug.Log (score);
+
+				} 
+		if (other.gameObject.name == "joint") {
+						score += 10;
+						Destroy (other.gameObject);
+						Debug.Log ("HUZZAH");
+				}
+		else
+						Debug.Log ("FUCCCCK");
+	}
+	
+}
